d9a7b3727b32a6ceb079be89433e5013fe7b5f86
Xerooon <ilyaliganov24@gmail.com>
2024-12-13 14:15:09 +0100

    minor fixes

--- codeC/avl.c
+++ codeC/avl.c
@@ -5,47 +5,47 @@
 
 // Function to create a new AVL tree node
 pAVL createNode(long id, long capacity, long consumption){
-  pAVL new = (pAVL)malloc(sizeof(AVL));
+  pAVL newNode = (pAVL)malloc(sizeof(AVL));
   // Check if memory allocation succeeded
-  if(!new){
-    fprintf(stderr, "Error allocating memory %s\n", sterror(errno));
+  if(!newNode){
+    fprintf(stderr, "Error allocating memory : %s\n", strerror(errno));
     exit(EXIT_FAILURE);
   }
 
-  new->id = id;
-  new->capacity = capacity;
-  new->consumption = consumption;
-  new->weight = 0;
-  new->left = NULL;
-  new->right = NULL;
+  newNode->id = id;
+  newNode->capacity = capacity;
+  newNode->consumption = consumption;
+  newNode->weight = 0;
+  newNode->left = NULL;
+  newNode->right = NULL;
 
-  return new;
+  return newNode;
 }
 
 // Function to perform a left rotation
 pAVL rotateLeft(pAVL station){
-  pAVL new = station->right;
-  station->right = new->left;
-  new->left = station;
+  pAVL newNode = station->right;
+  station->right = newNode->left;
+  newNode->left = station;
 
   // Update balance factors
-  station->weight = station->weight - 1 - (new->weight > 0 ? new->weight : 0);
-  new->weight = new->weight - 1 + (station->weight < 0 ? station->weight : 0);
+  station->weight = station->weight - 1 - (newNode->weight > 0 ? newNode->weight : 0);
+  newNode->weight = newNode->weight - 1 + (station->weight < 0 ? station->weight : 0);
 
-  return new;
+  return newNode;
 }
 
 // Function to perform a right rotation
 pAVL rotateRight(pAVL station){
-  pAVL new = station->left;
-  station->left = new->right;
-  new->right = station;
+  pAVL newNode = station->left;
+  station->left = newNode->right;
+  newNode->right = station;
 
   // Update balance factors
-  station->weight = station->weight + 1 - (new->weight < 0 ? new->weight : 0);
-  new->weight = new->weight + 1 + (station->weight > 0 ? station->weight : 0);
+  station->weight = station->weight + 1 - (newNode->weight < 0 ? newNode->weight : 0);
+  newNode->weight = newNode->weight + 1 + (station->weight > 0 ? station->weight : 0);
 
-  return new;
+  return newNode;
 }
 
 // Function to perform a double left rotation (right rotation followed by left rotation)
@@ -63,7 +63,7 @@ pAVL doubleRotateRight(pAVL station){
 // Function to balance an AVL tree nodes
 pAVL balanceAvl(pAVL station){
   if(station == NULL){
-    printf("Station empty\n");
+    printf("station not allocated in memory\n");
     return NULL;
   }
 
@@ -101,8 +101,14 @@ pAVL insertAVL(pAVL station, long id, long capacity, long consumption, int *h){
   else if(id > station->id){
     station->right = insertAVL(station->right, id, capacity, consumption, h);
     if(*h) station->weight++;
+  } 
+  // Duplicate ID case: ignore the new node
+  else {
+    *h = 0;
+    return station;
   }
 
+  // Balancing weight
   if(station->weight < -1 || station->weight > 1){
     station = balanceAvl(station);
     *h = 0;
@@ -118,7 +124,7 @@ pAVL insertAVL(pAVL station, long id, long capacity, long consumption, int *h){
 void printAvl(pAVL station){
   if(!station) return;
   printAvl(station->left);
-  printf("ID : %d| Capacity : %ld| Consumption : %ld\n", station->id, station->capacity, station->consumption);
+  printf("ID : %ld| Capacity : %ld| Consumption : %ld\n", station->id, station->capacity, station->consumption);
   printAvl(station->right);
 }
 
@@ -135,6 +141,6 @@ void freeAvl(pAVL station){
 void calculateConsumption(pAVL tree, FILE* outputFile){
   if(!tree) return;
   calculateConsumption(tree->left, outputFile);
-  fprintf(outputFile, "%d:%ld:%ld\n", tree->id, tree->capacity, tree->consumption);
+  fprintf(outputFile, "%ld:%ld:%ld\n", tree->id, tree->capacity, tree->consumption);
   calculateAndPrint(tree->right, outputFile);
 }
