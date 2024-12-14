d5e2bf099f2384d8461904e5df43871543983826
Xerooon <ilyaliganov24@gmail.com>
2024-12-14 20:46:48 +0100

    update : reading .csv + making AVL tree with station type

--- codeC/main.c
+++ codeC/main.c
@@ -1,32 +1,69 @@
-#include "avl.h"
-#include "fileOper.h"
 #include <stdio.h>
+#include "avl.h"
 
-int main(int argc, char* argv[]) {
+// Main fonction
+int main() {
+  pAVL root = NULL;
+  int h;
 
-  // Reading File
-  if(argc<2){
-    printf("Error: file not found\n");
-    return 0;
-  }
-  FILE* file = fopen(argv[1], "r");  
-  if(file==NULL){
-    printf("Error: opening file\n");
-    return 0;
+  // Open the CSV file
+  FILE *file = fopen("../tmp/stations.csv", "r");
+  if (file == NULL) {
+    perror("Erreur d'ouverture du fichier");
+    return 1;
   }
 
-  // Writing File
-  if(argc<3){
-    printf("Error: target not found\n");
-    return 0;
-  }
-  FILE* target = fopen(argv[2], "w"); 
-  if(target==NULL){
-    printf("Error: opening file\n");
-    return 0;
+  char line[256];
+  while (fgets(line, sizeof(line), file)) {
+    char *token;
+    long id = 0, capacity = 0, consumption = 0;
+    int column = 0;
+
+    token = strtok(line, ";");
+    while (token != NULL) {
+      switch (column) {
+        // HV-B ID field
+        case 1: 
+          if (strcmp(token, "-") != 0) {
+            id = atol(token);
+          }
+          break;
+        // HV-A ID field
+        case 2: 
+          if (strcmp(token, "-") != 0) {
+            id = atol(token);
+          }
+          break;
+        // LV ID field
+        case 3: 
+          if (strcmp(token, "-") != 0) {
+            id = atol(token);
+          }
+          break;
+        // Capacity field
+        case 6:
+          capacity = atol(token);
+          break;
+        // Consumption field
+        case 7:
+          consumption = atol(token);
+          break;
+        default:
+          break;
+      }
+      column++;
+      token = strtok(NULL, ";");
+    }
+
+    // Insert into the AVL tree if the station ID is valid (non-zero)
+    if (id != 0) {
+      root = insertAVL(root, id, capacity, consumption, &h);
+    }
   }
 
   fclose(file);
-  fclose(target);
+
+  printAvl(root);
+  freeAvl(root);
   return 0;
 }
